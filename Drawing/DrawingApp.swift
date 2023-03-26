412f1b2c87db1ab56dfdee9e6cde7bbed6f2c9d4
mberndt92 <maximilian_berndt@icloud.com>
2023-03-26 15:22:32 +0900

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Maximilian Berndt on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
