e2d4ebc414f56d240910e4138d90473509157ce5
mberndt92 <maximilian_berndt@icloud.com>
2023-03-29 09:13:58 +0900

    Make thickness animatable

--- Drawing/ArrorView.swift
+++ Drawing/ArrorView.swift
@@ -11,6 +11,12 @@ struct Arrow: InsettableShape {
     
     var insetAmount = 0.0
     
+    
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
@@ -19,18 +25,23 @@ struct Arrow: InsettableShape {
         
         let rectWidth = rect.width / 2
         
+        let maxY = rect.maxY - insetAmount
+        let maxX = rect.maxX - insetAmount
+        let minY = rect.minY + insetAmount
+        let minX = rect.minX + insetAmount
+        
         // Draw rectangle
-        path.move(to: CGPoint(x: rect.minX + rectWidth / 2, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX - rectWidth / 2, y: rect.maxY))
+        path.move(to: CGPoint(x: rect.minX + rectWidth / 2, y: maxY))
+        path.addLine(to: CGPoint(x: rect.maxX - rectWidth / 2, y: maxY))
         path.addLine(to: CGPoint(x: rect.maxX - rectWidth / 2, y: rect.minY + triangleHeight))
         path.addLine(to: CGPoint(x: rect.minX + rectWidth / 2, y: rect.minY + triangleHeight))
-        path.addLine(to: CGPoint(x: rect.minX + rectWidth / 2, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX + rectWidth / 2, y: maxY))
         
         // Draw triangle
         path.move(to: CGPoint(x: rect.minX + rectWidth / 2, y: rect.minY + triangleHeight))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + triangleHeight))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY + triangleHeight))
+        path.addLine(to: CGPoint(x: minX, y: rect.minY + triangleHeight))
+        path.addLine(to: CGPoint(x: rect.midX, y: minY))
+        path.addLine(to: CGPoint(x: maxX, y: rect.minY + triangleHeight))
         path.addLine(to: CGPoint(x: rect.minX + rectWidth / 2, y: rect.minY + triangleHeight))
         
         return path
@@ -45,10 +56,23 @@ struct Arrow: InsettableShape {
 }
 
 struct ArrorView: View {
+    
+    @State private var lineWidth = 5.0
+    
     var body: some View {
-        Arrow()
-            .frame(width: 300, height: 300)
+        VStack {
+            Arrow()
+                .strokeBorder(Color.green, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
             
+            Spacer()
+            
+            Button("Randomize line width") {
+                withAnimation {
+                    lineWidth = Double.random(in: 1...30)
+                }
+            }
+        }
+        .padding()
     }
 }
 
